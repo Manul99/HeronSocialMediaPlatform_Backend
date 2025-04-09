7413c515b384aacfbf80d48bf0599aadb26fe294
Manul99 <114052195+Manul99@users.noreply.github.com>
2025-04-09 21:12:23 +0530

    Created the docker file

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use('/api/parent',cors(),parentRoutes);
 app.use('/api/teacher',cors(),teacherRoutes);
 
 
-const PORT = 3001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0',() => console.log(`Server is running on port ${PORT}...`));
 
